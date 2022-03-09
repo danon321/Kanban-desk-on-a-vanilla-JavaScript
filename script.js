8f1f863ab281943cf130e295d06096f72670fc2a
Daniel Dobroczek <ddobroczek@primagran.local>
2022-03-09 23:29:19 +0100

    Add create new taskk functionality. Now its posible to chose title and description when you add new task or column

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let addColumnButton = document.getElementById('add_column');
+let columnNameInput = document.getElementById('column_name');
+let addColumnTask = document.getElementById('add_task');
+let taskTitleInput = document.getElementById('task_title');
+let taskDescInput = document.getElementById('task_desc');
 let columns = document.querySelectorAll('.column');
 let tasks = document.querySelectorAll('.task');
+
+let numberOfTasks = 0;
 
 //Drag and drop functions
 function onDragStart(event) {
@@ -15,42 +21,70 @@ function onDragStart(event) {
 }
 
 function onDragOver(event) {
-    console.log(event);
     event.preventDefault();
 }
 
 function onDrop(event) {
-    console.log(event);
     const id = event
         .dataTransfer
         .getData('text');
     const draggableElement = document.getElementById(id);
     const dropzone = event.target;
 
-    dropzone.appendChild(draggableElement);
+    // Make sure user don't drop element on another element
+    if(!dropzone.classList.contains("task") && !dropzone.parentElement.classList.contains("task"))
+        dropzone.appendChild(draggableElement);
+
     event
     .dataTransfer
     .clearData();
 }
 
+//Create elements
 function createNewColumn(){
     const parent = document.querySelector('.container');
     let column = document.createElement('div');
+    let title = document.createElement('p');
+
+    //Set new task name
+    columnNameInput.value ? title.innerHTML = columnNameInput.value : title.innerHTML = 'New Column';
 
     column.classList.add('column');
+    title.classList.add('column__title');
+
     column.addEventListener('dragover', onDragOver);
     column.addEventListener('drop', onDrop);
+
     parent.appendChild(column);
+    column.appendChild(title);
+}
+
+function createNewTask(){
+    const mainColumn = document.getElementById('main_column');
+    let task = document.createElement('div');
+    let title = document.createElement('p');
+    let desc = document.createElement('p');
+
+    //Set task content
+    taskTitleInput.value ? title.innerHTML = taskTitleInput.value : title.innerHTML = 'New Task';
+    taskDescInput.value ? desc.innerHTML = taskDescInput.value : desc.innerHTML = 'Description';
+
+    numberOfTasks++;
+
+    task.setAttribute("id", `draggable-${numberOfTasks}`);
+    task.classList.add('task');
+    task.setAttribute("draggable", true);
+
+    title.classList.add('task__title');
+    desc.classList.add('task__desc');
+
+    mainColumn.appendChild(task);
+    task.appendChild(title);
+    task.appendChild(desc);
+
+    task.addEventListener('dragstart', onDragStart);
 }
 
 //Events
 addColumnButton.addEventListener('click', createNewColumn);
-
-columns.forEach( column => {
-    column.addEventListener('dragover', onDragOver);
-    column.addEventListener('drop', onDrop);
-})
-
-tasks.forEach( task => {
-    task.addEventListener('dragstart', onDragStart);
-})
+addColumnTask.addEventListener('click', createNewTask);
